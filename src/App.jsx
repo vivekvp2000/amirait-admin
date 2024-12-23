f2cbe03e710f39d28d8249dcb3ececcde23fad2e
vivekvp2000 <vivekpayal999@gmail.com>
2024-12-23 18:33:44 +0530

    create login & register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-    <Routes>
+    <Routes basename="/">
       <Route path="/" element={<HomePage />}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/register" element={<RegisterPage />}></Route>
     </Routes>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -42,7 +42,7 @@ import {
             <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
               <a href="/" className="flex items-center gap-2 font-semibold">
                 <Package2 className="h-6 w-6" />
-                <span className="">Acme Inc</span>
+                <span className="">Amirait</span>
               </a>
               <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
                 <Bell className="h-4 w-4" />
@@ -63,49 +63,9 @@ import {
                   className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
                 >
                   <ShoppingCart className="h-4 w-4" />
-                  Orders
-                  <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                    6
-                  </Badge>
-                </a>
-                <a
-                  href="#"
-                  className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
-                >
-                  <Package className="h-4 w-4" />
-                  Products{" "}
-                </a>
-                <a
-                  href="#"
-                  className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                >
-                  <Users className="h-4 w-4" />
-                  Customers
-                </a>
-                <a
-                  href="#"
-                  className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                >
-                  <LineChart className="h-4 w-4" />
-                  Analytics
+                  Invoice
                 </a>
               </nav>
-            </div>
-            <div className="mt-auto p-4">
-              <Card x-chunk="dashboard-02-chunk-0">
-                <CardHeader className="p-2 pt-0 md:p-4">
-                  <CardTitle>Upgrade to Pro</CardTitle>
-                  <CardDescription>
-                    Unlock all features and get unlimited access to our support
-                    team.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-                  <Button size="sm" className="w-full">
-                    Upgrade
-                  </Button>
-                </CardContent>
-              </Card>
             </div>
           </div>
         </div>
@@ -136,14 +96,14 @@ import {
                     className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
                   >
                     <Home className="h-5 w-5" />
-                    Dashboard
+                    Dashboard...
                   </a>
                   <a
                     href="#"
                     className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
                   >
                     <ShoppingCart className="h-5 w-5" />
-                    Orders
+                    Invoice
                     <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
                       6
                     </Badge>
@@ -167,7 +127,7 @@ import {
                     className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
                   >
                     <LineChart className="h-5 w-5" />
-                    Analytics
+                    Analytics..
                   </a>
                 </nav>
                 <div className="mt-auto">
@@ -219,7 +179,7 @@ import {
           </header>
           <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
             <div className="flex items-center">
-              <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
+              <h1 className="text-lg font-semibold md:text-2xl">Home</h1>
             </div>
             <div
               className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
